d32824ddecf6950ad25cc87b640a4dbdd9130914
Kilderson Sena <dersonsena@gmail.com>
2018-06-06 23:59:55 -0300

    Terceira aula de security

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     * @return Response
+     */
+    public function admin()
+    {
+        return new Response('<html><body><h1>Admin Page!</h1></body></html>');
+    }
+}
